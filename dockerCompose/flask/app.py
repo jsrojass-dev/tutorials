f2f1e529d90b923dd9e9660e424340f0850a3777
Juan Sebastian Rojas <juansrojass@outlook.com>
2024-11-30 17:19:43 -0500

    Docker compose passing env variables and secrets (#3)

--- dockerCompose/flask/app.py
+++ dockerCompose/flask/app.py
@@ -9,3 +9,13 @@ def about():
     version = os.environ.get('APP_VERSION')
 
     return {'version': version}, 200
+
+@app.route('/secrets', methods=['GET'])
+def secrets():
+    credentials = dict()
+    credentials['db_password'] = os.environ.get('DB_PASSWORD')
+    credentials['app_token'] = os.environ.get('APP_TOKEN')
+    credentials['api_key'] = open("/run/secrets/api_key","r").read()
+    credentials['api_key_v2'] = open("/api_key.txt","r").read()
+
+    return credentials, 200
